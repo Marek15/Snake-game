4af3002101c14c38cde85112460dd86446f64317
Marek15 <marekhth@gmail.com>
2021-03-17 13:32:32 +0100

    added snake

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,27 +11,41 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    private int ROWS = 15, COLUMNS = 15, SQUARE_SIZE = 40;
+    private int ROWS = 15;
+    private int COLUMNS = 15;
+    private int SQUARE_SIZE = 40;
 
     private GraphicsContext gc;
+
+
+    private Snake snake;
+
+
     @FXML
     Canvas background;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         gc = background.getGraphicsContext2D();
+
+        snake = new Snake();
         drawBackground(gc);
-
     }
-
-
-
 
 
     private void drawBackground(GraphicsContext gc){
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLUMNS; j++) {
-                if (i == 10 && j == 10 || i == 4 && j == 2 || i == 8 && j == 5){
+                if (snake.getTail().getX() == i && snake.getTail().getY() == j){
+                    gc.setFill(Color.web("ffffff"));
+                }
+                else if(snake.getHead().getX() == i && snake.getHead().getY() == j){
+                    gc.setFill(Color.web("aaaaaa"));
+                }
+                else if (snake.getBody().get(0).getX() == i && snake.getBody().get(0).getY() == j){
+                    gc.setFill(Color.web("f4f4f4"));
+                }
+                else if (i == 10 && j == 10 || i == 4 && j == 2 || i == 8 && j == 5){
                     gc.setFill(Color.web("fc052a"));
                 }
                 else if ((i+j) % 2 == 0){
@@ -44,4 +58,5 @@ public class Controller implements Initializable {
             }
         }
     }
+
 }
---
+++ src/sample/Points.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Points {
+
+    ArrayList<Point> grid = new ArrayList();
+
+    public Points(ArrayList<Point> grid) {
+        this.grid = grid;
+    }
+}
